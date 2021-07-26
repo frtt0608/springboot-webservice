528de2c42e9c78f1559277a670a79a67574caae1
Heon_9u <frtt0608@naver.com>
2021-07-26 13:17:53 +0900

    210726 | 오타 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/evn bash
 
-function  find_idle_profile() {
+function find_idle_profile() {
   RESPONSE_CODE=$(curl -s -o /dev/null -w "#{http_code}" http://localhost/profile)
 
   if [ ${RESPONSE_CODE} -ge 400 ]
@@ -15,14 +15,14 @@ function  find_idle_profile() {
   then
     IDLE_PROFILE=real2
   else
-    IDEL_PROFILE=real1
+    IDLE_PROFILE=real1
   fi
 
-  echo "${IDEL_PROFILE}"
+  echo "${IDLE_PROFILE}"
 }
 
 function find_idle_port() {
-  IDEL_PROFILE=$(find_idle_profile)
+  IDLE_PROFILE=$(find_idle_profile)
 
   if [ ${IDLE_PROFILE} == real1 ]
   then
