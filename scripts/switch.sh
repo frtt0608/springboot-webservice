871d6756ad0d38003975fb55d3e9ec7171a94ca1
Heon_9u <frtt0608@naver.com>
2021-07-26 13:07:54 +0900

    210726 | 무중단 배포 테스트|배포 스크립트 작성|작동 중인 상태에 따라 profile 변경

---
+++ scripts/switch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+function switch_proxy() {
+    IDLE_PORT=$(find_idle_profile)
+
+    echo "> 전환활 port: $IDLE_PORT"
+    echo "> Port 전환"
+    echo "set \$service_url http://127.0.0.1:{$IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+
+    echo "> 엔진엑스 Reload"
+    sudo service nginx reload
+}
