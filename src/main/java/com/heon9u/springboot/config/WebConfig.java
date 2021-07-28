aed200cab2f91a27a53735487e0a3a252101609a
Heon_9u <frtt0608@naver.com>
2021-07-28 21:54:01 +0900

    210728 | 들여쓰기 정리

--- src/main/java/com/heon9u/springboot/config/WebConfig.java
+++ src/main/java/com/heon9u/springboot/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.heon9u.springboot.config.auth.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
